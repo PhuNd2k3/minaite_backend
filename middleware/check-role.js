20cbcf359812ef0f69ab503c35d25529c283edaf
dungnvf-0423 <139527315+dungnvf-0423@users.noreply.github.com>
2023-08-29 12:59:03 +0700

    middleware original

--- middleware/check-role.js
+++ middleware/check-role.js
@@ -3,47 +3,48 @@ const ADMIN_ROLE = true;
 const jwt = require("jsonwebtoken");
 
 async function checkRoleUser(request, response, next) {
-  try {
-    const token = request.headers.authorization.split(' ')[1]
-    console.log(token)
-    const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const requestRole = decode.isAdmin;
+    try {
+        const token = request.headers.authorization.split(" ")[1];
+        console.log(token);
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const requestRole = decode.isAdmin;
 
-    // Check if request user is admin or not
-    if (requestRole != USER_ROLE) {
-      return response.status(400).json({
-        message: "Invalid role!",
-      });
-    } else next();
-
-  } catch (error) {
-    return response.status(401).json({
-      message: "Something went wrong!",
-      error: error,
-    });
-  }
+        // Check if request user is admin or not
+        if (requestRole != ADMIN_ROLE) {
+            if (requestRole != USER_ROLE) {
+                return response.status(400).json({
+                    message: "Invalid role!",
+                });
+            } else next();
+        } else next();
+    } catch (error) {
+        return response.status(401).json({
+            message: "Something went wrong!",
+            error: error,
+        });
+    }
 }
 
 async function checkRoleAdmin(request, response, next) {
-  try {
-    const token = request.headers.authorization.split(" ")[1];
-    const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    const requestRole = decode.isAdmin;
-    // Check if request user is admin or not
-    if (requestRole != ADMIN_ROLE) {
-      return response.status(400).json({
-        message: "Invalid role!",
-      });
-    } else next();
-  } catch (error) {
-    return response.status(401).json({
-      message: "Something went wrong!",
-      error: error,
-    });
-  }
+    try {
+        const token = request.headers.authorization.split(" ")[1];
+        const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const requestRole = decode.isAdmin;
+        // Check if request user is admin or not
+        if (requestRole != ADMIN_ROLE) {
+            return response.status(400).json({
+                message: "Invalid role!",
+            });
+        } else next();
+    } catch (error) {
+        return response.status(401).json({
+            message: "Something went wrong!",
+            error: error,
+        });
+    }
 }
 
 module.exports = {
-  checkRoleUser: checkRoleUser,
-  checkRoleAdmin: checkRoleAdmin,
+    checkRoleUser: checkRoleUser,
+    checkRoleAdmin: checkRoleAdmin,
 };
