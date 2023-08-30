4757e937f29b65bc3b16b13187ca807a3c66f753
dungnvf-0423 <139527315+dungnvf-0423@users.noreply.github.com>
2023-08-30 14:16:58 +0700

    insert into app

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(express.json());
 
 const staticPath = path.join(__dirname, 'public');
 
+app.use('/',(req,res) => {
+    res.send('Shopee_api')
+})
+
 app.use(express.static(staticPath));
 app.use('/public', express.static('public'))
 
